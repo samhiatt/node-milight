1808d5e30c8708db03c3c9f3eb16dd39f625afeb
Sam Hiatt <sam.hiatt@gmail.com>
2015-05-21 02:08:05 -0700

    removed unneeded listeners

--- udpserver.js
+++ udpserver.js
@@ -6,30 +6,14 @@ exports.createServer = function(cb){
 	var server = dgram.createSocket("udp4");
 
 	server.on("error", function (err) {
-		console.log("server error:\n" + err.stack);
+		console.error("server error:\n" + err.stack);
 		server.close();
-	});
-
-	server.on("message", function (msg, rinfo) {
-		var msgArr = [];
-		for (var i=0; i<msg.length; i++){
-			msgArr.push(msg[i]);
-		}
-		console.log("server got: " + msgArr + " from " +
-		rinfo.address + ":" + rinfo.port);
-	});
-
-	server.on("listening", function () {
-		var address = server.address();
-		console.log("\nserver listening " +
-		address.address + ":" + address.port);
 	});
 
 	server.bind(8899);
 	
 	cb(null,server);
 	return server;
-	
 };
 
 
