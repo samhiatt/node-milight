740ff201b3172471337ae0f236c5a1b4da6f6669
Sam Hiatt <sam.hiatt@gmail.com>
2015-05-20 20:46:49 -0700

    working on unit tests

--- test.js
+++ test.js
@@ -1,21 +1,122 @@
-var controller = require('./')({host:'192.168.42.100'}).WhiteController;
+var milight = require('./')({host:'localhost'});
+var udpserver = require('./udpserver');
+var async = require('async');
 
-//controller.on(1,function(err,res){
-//	if (err) throw err;
-//	console.log("RESPONSE:",res);
-	//setTimeout(function(){
-	//	controller.off(1,function(err,res){
-	//		if (err) throw err;
-	//		console.log(" RESPONSE:",res);
-	//		process.exit();
-	//	});
-	//},100);
-//});
+exports.setUp = function (callback) {
+	udpserver.createServer(function(err,server){
+		this.server = server;
+		callback();
+	}.bind(this));
+};
+exports.tearDown = function (callback) {
+	this.server.close();
+	callback();
+};
 
-//controller.off(1, function(err,res){
-//	if (err) throw err;
-//	console.log("GROUP 1 OFF RESPONSE:", res);
-//	//process.exit();
-//});
 
-controller.brightness(10,4)
+var getMsgArray = function(msg){
+	var msgArr = [];
+	for (var i=0; i<msg.length; i++){
+		msgArr.push(msg[i]);
+	}
+	return msgArr;
+};
+
+function testCommand(command,group,expected,server,test,callback){
+	async.parallel([
+		function(cb){
+			server.once("message", function (msg, rinfo) {
+				var msgArr = getMsgArray(msg);
+				test.equal(msgArr[0], expected, "Expected response for '"+command+"' group "+group+" command: msgArr[0]=="+expected);
+				cb();
+			});}.bind(this),
+		function(callback){
+			var cb = function(err,res){
+				if (err) throw err;
+				test.equal(res,3,"Expected UDP response: 3");
+				callback();
+			};
+			if (group != null) milight.WhiteController[command](group,cb);
+			else milight.WhiteController[command](null,cb);
+		}.bind(this),
+	],callback);
+};
+
+exports.testWhite = {
+	testOnOff: function(test){
+		var server = this.server;
+		async.series([
+			function(cb){
+				testCommand('on',0,0x35,server,test,cb);
+			},
+			function(cb){
+				testCommand('off',0,0x39,server,test,cb);
+			},
+			function(cb){
+				testCommand('on',1,0x38,server,test,cb);
+			},
+			function(cb){
+				testCommand('off',1,0x3b,server,test,cb);
+			},
+			function(cb){
+				testCommand('on',2,0x3d,server,test,cb);
+			},
+			function(cb){
+				testCommand('off',2,0x33,server,test,cb);
+			},
+			function(cb){
+				testCommand('on',3,0x37,server,test,cb);
+			},
+			function(cb){
+				testCommand('off',3,0x3a,server,test,cb);
+			},
+			function(cb){
+				testCommand('on',4,0x32,server,test,cb);
+			},
+			function(cb){
+				testCommand('off',4,0x36,server,test,cb);
+			},
+		],function(err,res){
+			if (err) throw err;
+			test.done();
+		});
+	},
+	testBrightnessUpDown: function(test){
+		var server = this.server;
+		async.series([
+			function(cb){
+				testCommand('brightnessUp',null,0x3c,server,test,cb);
+			},
+			function(cb){
+				testCommand('brightnessDown',null,0x34,server,test,cb);
+			},
+			//function(cb){
+			//	testCommand('brightnessUp',1,0x38,server,test,cb);
+			//},
+			//function(cb){
+			//	testCommand('brightnessDown',1,0x3b,server,test,cb);
+			//},
+			//function(cb){
+			//	testCommand('brightnessUp',2,0x3d,server,test,cb);
+			//},
+			//function(cb){
+			//	testCommand('brightnessDown',2,0x33,server,test,cb);
+			//},
+			//function(cb){
+			//	testCommand('brightnessUp',3,0x37,server,test,cb);
+			//},
+			//function(cb){
+			//	testCommand('brightnessDown',3,0x3a,server,test,cb);
+			//},
+			//function(cb){
+			//	testCommand('brightnessUp',4,0x32,server,test,cb);
+			//},
+			//function(cb){
+			//	testCommand('brightnessDown',4,0x36,server,test,cb);
+			//},
+		],function(err,res){
+			if (err) throw err;
+			test.done();
+		});
+	}
+};
